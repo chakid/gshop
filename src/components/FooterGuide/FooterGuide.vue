8ceb5975ee00e08272d57f7bf8963771725e3f94
yuanyonggui <yuanyonggui@annetinfo.com>
2019-09-19 22:13:42 +0800

    尚硅谷外卖项目首次提交

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        APP
+        FooterGuide
     </div>
 </template>
 <script>
--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        APP
+        Search
     </div>
 </template>
 <script>
